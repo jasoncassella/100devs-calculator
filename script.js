20099d79aeb50d5f7f172bf777691b0ec83d5926
Jason Cassella <jasoncassella1@gmail.com>
2022-04-08 11:36:10 -0400

    need to make it so you cant enter 2 decimals

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 /*
-required abilities of a calculator:
-accept user inputs of number, operator, and number
-accept decimal numbers
-store inputs
-recognize inputs and preform calculations
-return a result
-
 optional features:
-should accept longer artithmetic operations.
-clear when operand is pressed and display all inputs in the upper right in small font
-store previous total as start of next operation
-all clear button should clear all entries
-should prevent invalid inputs (operators next to each other, two decimal points, etc.)
+- should prevent invalid inputs (operators next to each other, two decimal points, etc.)
 */
 
 const keys = document.querySelector('.calculator-buttons');
@@ -19,8 +8,58 @@ keys.addEventListener('click', e => {
 	const { target } = e;
 	const { value } = target;
 	if (!target.matches('button')) return;
-	else {
-		// pass value to parse method
-		console.log(value);
-	}
+	else caluclator.parseInput(value);
 });
+
+const caluclator = {
+	displayText: '0',
+	prevTotal: null,
+
+	parseInput(value) {
+		switch (value) {
+			case '=':
+				this.calcAnswer(this.displayText);
+				break;
+			case 'AC':
+				this.clearAll();
+				break;
+			case '.':
+				if (this.displayText === '0') {
+					this.addText('0.');
+				} else {
+					this.addText(value);
+				}
+				break;
+			default:
+				this.addText(value);
+		}
+	},
+
+	addText(value) {
+		if (this.displayText === '0') this.displayText = '';
+		else if (this.prevTotal !== null) {
+			this.displayText = this.prevTotal;
+			this.prevTotal = null;
+		}
+		if (isNaN(+value) && isNaN(+this.displayText)) {
+			if (isNaN(this.displayText.slice(-1))) return;
+		}
+		this.displayText += value;
+		this.outputText(this.displayText);
+	},
+
+	outputText(text) {
+		document.querySelector('.calculator-screen').value = text;
+	},
+
+	calcAnswer(equation) {
+		let result = Function('return ' + equation)();
+		this.outputText(result);
+	},
+
+	clearAll() {
+		this.displayText = '0';
+		this.prevTotal = null;
+		this.outputText(this.displayText);
+	},
+};
